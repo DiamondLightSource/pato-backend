c93c4ce3f8a1a47d5cc067a1461b244c6897755e
Guilherme de Freitas <guilherme.de-freitas@diamond.ac.uk>
2022-12-07 10:12:05 +0000

    Fixes tests

--- tests/users.py
+++ tests/users.py
@@ -1,15 +1,30 @@
 from ebic.auth.template import GenericUser
 
 admin = GenericUser(
-    id=00, family_name="Admin", title="Dr.", given_name="McAdmin", permissions=[11]
+    fedid="admin",
+    id=00,
+    family_name="Admin",
+    title="Dr.",
+    given_name="McAdmin",
+    permissions=[11],
 )
 
 em_admin = GenericUser(
-    id=18660, family_name="EM", title="Dr.", given_name="Admin", permissions=[8]
+    fedid="em_admin",
+    id=18660,
+    family_name="EM",
+    title="Dr.",
+    given_name="Admin",
+    permissions=[8],
 )
 
 user = GenericUser(
-    id=18600, family_name="Generic", title="Dr.", given_name="User", permissions=[]
+    fedid="user",
+    id=18600,
+    family_name="Generic",
+    title="Dr.",
+    given_name="User",
+    permissions=[],
 )
 
 
