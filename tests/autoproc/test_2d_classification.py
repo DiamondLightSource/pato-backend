03b763fcbc9485e127bb309abbc2ef45c12b0b73
Guilherme de Freitas <guilherme.de-freitas@diamond.ac.uk>
2023-01-10 16:53:38 +0000

    Adds image endpoint tests

--- tests/autoproc/test_2d_classification.py
+++ tests/autoproc/test_2d_classification.py
@@ -1,4 +1,8 @@
+from unittest.mock import patch
+
 import pytest
+
+from ..conftest import mock_send
 
 
 @pytest.mark.parametrize("mock_permissions", [200], indirect=True)
@@ -45,4 +49,37 @@ def test_get_forbidden(mock_permissions, client):
     """Try to get particle picking data for an autoprocessing program where the user is
     not present in the parent session"""
     resp = client.get("/autoProc/56986680/classification")
+    assert resp.status_code == 403
+
+
+@pytest.mark.parametrize("mock_permissions", [200], indirect=True)
+def test_get_image(mock_permissions, client):
+    """Get particle picking summary image"""
+    with patch("ebic.routes.movies.FileResponse.__call__", new=mock_send):
+        resp = client.get("/autoProc/56986680/classification/1/image")
+    assert resp.status_code == 200
+
+
+@pytest.mark.parametrize("mock_permissions", [200], indirect=True)
+def test_get_image_not_in_db(mock_permissions, client):
+    """Get particle picking summary image not in database"""
+    with patch("ebic.routes.movies.FileResponse.__call__", new=mock_send):
+        resp = client.get("/autoProc/56986680/classification/999/image")
+    assert resp.status_code == 404
+
+
+@pytest.mark.parametrize("mock_permissions", [200], indirect=True)
+def test_get_image_not_found(mock_permissions, exists_mock, client):
+    """Get particle picking summary image not in filesystem"""
+    exists_mock.return_value = False
+    resp = client.get("/autoProc/56986680/classification/5/image")
+    assert resp.status_code == 404
+
+
+@pytest.mark.parametrize("mock_permissions", [403], indirect=True)
+def test_get_image_forbidden(mock_permissions, client):
+    """Try to get particle picking summary image for user not present
+    in the parent session"""
+    with patch("ebic.routes.movies.FileResponse.__call__", new=mock_send):
+        resp = client.get("/autoProc/56986680/classification/1/image")
     assert resp.status_code == 403
--- tests/conftest.py
+++ tests/conftest.py
@@ -57,9 +57,3 @@ def mock_permissions(request):
 def exists_mock():
     with patch("ebic.utils.generic.isfile", return_value=True) as _fixture:
         yield _fixture
-
-
-@pytest.fixture(scope="module", autouse=True)
-def file_response_mock():
-    with patch("ebic.routes.movies.FileResponse.__call__", new=mock_send) as _fixture:
-        yield _fixture
