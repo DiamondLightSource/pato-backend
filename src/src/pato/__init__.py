6f16019612a6833b8b725f28906c53d81a454788
Guilherme Francisco <guilherme.de-freitas@diamond.ac.uk>
2023-03-29 11:08:15 +0100

    Delete src/src/pato directory

--- src/src/pato/__init__.py
+++
@@ -1,5 +0,0 @@
-from .utils.config import Config
-
-__version__ = "0.5.0"
-
-Config.set()
--- src/src/pato/models/parameters.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-
-class ReprocessingParameters(BaseModel):
-    pixelSize: int
-    tiltOffset: float
