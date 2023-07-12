09dffcdfb9fb6bda34f76c4e6e1549d8d9ab4dbc
Guilherme de Freitas <guilherme.de-freitas@diamond.ac.uk>
2023-07-12 15:52:12 +0100

    Fix session has person filter

--- src/pato/utils/auth.py
+++ src/pato/utils/auth.py
@@ -1,7 +1,7 @@
-from sqlalchemy import Select, or_
+from sqlalchemy import Select, and_, or_
 
 from ..auth import User, is_admin
-from ..models.table import BLSession, ProposalHasPerson, SessionHasPerson
+from ..models.table import BLSession, Proposal, ProposalHasPerson, SessionHasPerson
 from .config import Config
 
 
@@ -19,11 +19,18 @@ def check_session(query: Select, user: User):
     if is_admin(user.permissions):
         return query
 
-    return query.join(SessionHasPerson, isouter=True).filter(
-        or_(
-            BLSession.beamLineName.in_(get_allowed_beamlines(user.permissions)),
+    return query.join(
+        SessionHasPerson,
+        and_(
+            SessionHasPerson.sessionId == BLSession.sessionId,
             SessionHasPerson.personId == user.id,
-        )
+        ),
+        isouter=True,
+    ).filter(
+        or_(
+            SessionHasPerson.personId == user.id,
+            BLSession.beamLineName.in_(get_allowed_beamlines(user.permissions)),
+        ),
     )
 
 
@@ -31,9 +38,16 @@ def check_proposal(query: Select, user: User):
     if is_admin(user.permissions):
         return query
 
-    return query.join(ProposalHasPerson, isouter=True).filter(
-        or_(
-            BLSession.beamLineName.in_(get_allowed_beamlines(user.permissions)),
+    return query.join(
+        ProposalHasPerson,
+        and_(
             ProposalHasPerson.personId == user.id,
-        )
+            ProposalHasPerson.proposalId == Proposal.proposalId,
+        ),
+        isouter=True,
+    ).filter(
+        or_(
+            ProposalHasPerson.personId == user.id,
+            BLSession.beamLineName.in_(get_allowed_beamlines(user.permissions)),
+        ),
     )
