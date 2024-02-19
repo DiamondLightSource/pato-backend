b64be844d2e10b3ca59bbda7f0ccb479967f9097
Guilherme de Freitas <guilherme.de-freitas@diamond.ac.uk>
2024-02-19 15:47:16 +0000

    Remove extra join conditions on outer join in auth

--- src/pato/utils/auth.py
+++ src/pato/utils/auth.py
@@ -1,5 +1,5 @@
 from lims_utils.tables import BLSession, Proposal, ProposalHasPerson, SessionHasPerson
-from sqlalchemy import Select, and_, or_
+from sqlalchemy import Select, or_
 
 from ..auth import User, is_admin
 from .config import Config
@@ -21,15 +21,13 @@ def check_session(query: Select, user: User):
 
     allowed_beamlines = get_allowed_beamlines(user.permissions)
     or_expressions = [SessionHasPerson.personId == user.id]
-    join_conditions = [SessionHasPerson.sessionId == BLSession.sessionId]
 
     if allowed_beamlines:
         or_expressions.append(BLSession.beamLineName.in_(allowed_beamlines))
-        join_conditions.append(SessionHasPerson.personId == user.id)
 
     return query.join(
         SessionHasPerson,
-        and_(*join_conditions),
+        SessionHasPerson.sessionId == BLSession.sessionId,
         isouter=(len(allowed_beamlines) > 0),
     ).filter(or_(*or_expressions))
 
@@ -40,15 +38,13 @@ def check_proposal(query: Select, user: User):
 
     allowed_beamlines = get_allowed_beamlines(user.permissions)
     or_expressions = [ProposalHasPerson.personId == user.id]
-    join_conditions = [ProposalHasPerson.proposalId == Proposal.proposalId]
 
     if allowed_beamlines:
         or_expressions.append(BLSession.beamLineName.in_(allowed_beamlines))
-        join_conditions.append(ProposalHasPerson.personId == user.id)
 
     return query.join(
         ProposalHasPerson,
-        and_(*join_conditions),
+        ProposalHasPerson.proposalId == Proposal.proposalId,
         isouter=(len(allowed_beamlines) > 0),
     ).filter(
         or_(*or_expressions),
