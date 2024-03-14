ccbbfb5bb41b157e26485685a6af1d8a77461bf0
Guilherme de Freitas <guilherme.de-freitas@diamond.ac.uk>
2024-03-14 16:28:54 +0000

    Release v1.4.1

--- src/pato/crud/proposals.py
+++ src/pato/crud/proposals.py
@@ -1,16 +1,16 @@
+from lims_utils.auth import GenericUser
 from lims_utils.models import Paged
 from lims_utils.tables import BLSession, Proposal
 from sqlalchemy import func as f
 from sqlalchemy import or_, select
 
-from ..auth import User
 from ..models.response import ProposalResponse
 from ..utils.auth import check_proposal
 from ..utils.database import paginate
 
 
 def get_proposals(
-    limit: int, page: int, search: str, user: User
+    limit: int, page: int, search: str, user: GenericUser
 ) -> Paged[ProposalResponse]:
     cols = [c for c in Proposal.__table__.columns if c.name != "externalId"]
     query = (
