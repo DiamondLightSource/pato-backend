87680ef3baa8d81638d9ef970709fc650814d4a1
Guilherme de Freitas <guilherme.de-freitas@diamond.ac.uk>
2024-08-07 08:37:58 +0100

    Add extra check to test

--- tests/collections/test_ctf_defocus.py
+++ tests/collections/test_ctf_defocus.py
@@ -2,5 +2,8 @@ def test_get(mock_permissions, client):
     """Get defocus/particle count data for data collection"""
     resp = client.get("/dataCollections/6017412/ctf")
     assert resp.status_code == 200
-    assert resp.json()["items"][0]["x"] == 5
-    assert resp.json()["items"][0]["y"] == 10
+
+    items = resp.json()["items"]
+    assert items[0]["x"] == 5
+    assert items[0]["y"] == 10
+    assert len(items) == 1
