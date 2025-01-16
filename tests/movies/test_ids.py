8ed2ef51c1f0a0308d261610a2e8ff398f245d4b
James Gilbert <james.gilbert@diamond.ac.uk>
2025-01-16 11:44:38 +0000

    Remove unnecessary import

--- tests/movies/test_ids.py
+++ tests/movies/test_ids.py
@@ -1,9 +1,6 @@
-from ..conftest import mock_send
-
-
 def test_get_movie_info(mock_permissions, client):
     """Get foil hole and grid square IDs for a given movie"""
-      resp = client.get("/movies/27")
+    resp = client.get("/movies/27")
     assert resp.status_code == 200
     assert resp.json()["foilHoleId"] == 1
     assert resp.json()["gridSquareId"] == 1
