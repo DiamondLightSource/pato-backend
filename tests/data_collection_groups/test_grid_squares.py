9c07d8ccc6986549423d418172de8d8a99eb5146
James Gilbert <james.gilbert@diamond.ac.uk>
2025-02-06 10:09:44 +0000

    Add test for get uncollected grid squares

--- tests/data_collection_groups/test_grid_squares.py
+++ tests/data_collection_groups/test_grid_squares.py
@@ -3,3 +3,9 @@ def test_get(mock_permissions, client):
     resp = client.get("/dataGroups/5440742/grid-squares")
     assert resp.status_code == 200
     assert len(resp.json()["items"]) == 1
+
+def test_get_empty(mock_permissions, client):
+    """Get all grid squares (including uncollected)"""
+    resp = client.get("/dataGroups/5440742/grid-squares", params="hideUncollected=false")
+    assert resp.status_code == 200
+    assert len(resp.json()["items"]) == 2
